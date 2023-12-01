b3562f1cb9f74f383487fe38b14fcf2a6d6fc514
01001110-01100001-01101110-01100101 <nane.akopyan.2004@mail.ru>
2023-12-01 14:14:57 +0300

    активация 1 игры

--- script_site.js
+++ script_site.js
@@ -1,18 +1,17 @@
 const month = (num) => {
     if (num === '1' || num === '2' || num === '12') {
-        console.log("Зима");
+        alert ("Зима");
     } 
     else if (num === '3' || num === '4' || num === '5') {
-        console.log("Весна");
+        alert("Весна");
     } 
     else if (num === '6' || num === '7' || num === '8') {
-        console.log("Лето");
+        alert("Лето");
     } 
     else if (num === '9' || num === '10' || num === '11') {
-        console.log("Осень");
+        alert("Осень");
     }
     else {
-        console.log("Вы ввели неправильное значение");
+        alert("Вы ввели неправильное значение");
     }
 };
-month(prompt("Введите номер месяца"));
